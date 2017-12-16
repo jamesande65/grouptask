2d6a517f1a4359a59104835f3737804933be9786
kuznetsovandrew <30andrey14@gmail.com>
2017-12-16 13:41:59 +0200

    create logic for list of songs

---
+++ js/list.js
@@ -0,0 +1,42 @@
+let buttonAdd = document.querySelector('.add');
+let fileInput = document.querySelector('#load-file');
+let listSongs = document.querySelector('.list_songs');
+
+buttonAdd.addEventListener('click', chooseSong);
+fileInput.addEventListener('change', addSong);
+
+
+function chooseSong() {
+    fileInput.click();
+}
+
+function addSong() {
+    let song = fileInput.value;
+    fileInput.value = '';
+    addToLocalStorage(song);
+    synchrPlayList();
+}
+
+function addToLocalStorage(song) {
+    let playList = localStorage.getItem('playList');
+    if(playList){
+        playList = JSON.parse(playList);
+        playList.push(song);
+    }else{
+        playList = [];
+        playList.push(song)
+    }
+    playList = JSON.stringify(playList);
+    localStorage.setItem('playList', playList);
+}
+
+function synchrPlayList(song) {
+    let playList = localStorage.getItem('playList');
+    listSongs.innerHTML = '';
+    playList = JSON.parse(playList);
+    if(playList){
+        playList.forEach((song)=>{
+            listSongs.insertAdjacentHTML('beforeEnd', '<li>' + song + '</li>');
+        });
+    }
+}
