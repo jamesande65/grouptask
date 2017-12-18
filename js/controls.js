72da9c0e80419464ae80a0586d697504cf5850c8
jamesande65 <james.ande65@gmail.com>
2017-12-18 11:55:09 +0200

    последние изменения

--- js/controls.js
+++ js/controls.js
@@ -1,32 +1,54 @@
-let toggle = document.querySelector('.toggle');
+let playButton = document.querySelector('.audio__button');
+let stopButton = document.querySelector('.player__button');
 let progressLine = document.querySelector('.progress__filled');
 let progressParent = document.querySelector('.progress');
-let audio = document.querySelector('.audio__link');
+let theAudio = document.querySelector('audio');
+
+
 
 
 function togglePlay(ev) {
-    let method = audio.paused ? 'play': 'pause';
-    audio[method]();
+    let method = theAudio.paused ? 'play': 'pause';
+    theAudio[method]();
 }
 
 function progress() {
-    const persent = (audio.currentTime / audio.duration ) * 100;
+    const persent = (theAudio.currentTime / theAudio.duration ) * 100;
     progressLine.style.flexBasis = `${persent}%`;
 }
 
 function handleProgress(ev) {
-    let current = (ev.offsetX / progressParent.offsetWidth) * audio.duration;
-    audio.currentTime = current;
+    let current = ((ev.offsetX / progressParent.offsetWidth) * theAudio.duration);
+    setCurrentTime(current);
+}
+
+function setCurrentTime(current) {
+    if (!isNaN(theAudio.duration)) {
+        theAudio.currentTime = current;
+    }
+    console.log(theAudio);
+    console.log(theAudio.duration);
+    console.log(theAudio.currentTime);
 }
 
 function togglePlayButton() {
-    let icon = audio.paused ? '►' : '❚❚';
-    toggle.innerHTML = icon;
+    playButton.innerHTML = theAudio.paused ? '►' : '❚❚';
 }
 
-toggle.addEventListener('click', togglePlay);
-audio.addEventListener('play', togglePlayButton);
-audio.addEventListener('pause', togglePlayButton);
+function stopAudio() {
+    let method = 'pause';
+    theAudio[method]();
+    theAudio.load();
+    progressLine.style.flexBasis = 0;
+}
 
-audio.addEventListener('timeupdate', progress);
+playButton.addEventListener('click', togglePlay);
+stopButton.addEventListener('click', stopAudio);
+stopButton.addEventListener('click', togglePlayButton);
+theAudio.addEventListener('click', togglePlay);
+theAudio.addEventListener('play', togglePlayButton);
+theAudio.addEventListener('pause', togglePlayButton);
+
+theAudio.addEventListener('timeupdate', progress);
 progressParent.addEventListener('mousedown', handleProgress);
+
